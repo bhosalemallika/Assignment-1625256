1284b64da15ca98840834bf358b1d005bbcb8886
Mallika Bhosale <102364891+bhosalemallika@users.noreply.github.com>
2022-03-25 20:56:05 +0530

    Create arrayminmax.java

---
+++ arrayminmax.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main 
+{
+ public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements you want in an array");
+        int n = sc.nextInt();
+        
+        System.out.println("Enter elements in the array");
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++) 
+        {
+          arr[i] = sc.nextInt();
+        }
+        
+        int minimum = arr[0];
+        int maximum = arr[0];
+        for(int i=0; i<n; i++) 
+        {
+            if(minimum > arr[i])
+                minimum = arr[i];
+            if(maximum < arr[i])
+                maximum = arr[i];
+        }
+        
+        System.out.println("Minimum value in an array is:" + minimum);
+        System.out.println("Maximum value in an array is:" + maximum);
+    }
+
+}
